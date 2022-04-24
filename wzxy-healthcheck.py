ea410be4d71fbe2f03fe86db3a0fe079e0574e81
MIKUCHINCHAN <90995793+MIKUCHINCHAN@users.noreply.github.com>
2022-04-24 21:48:49 +0800

    Update wzxy-healthcheck.py

--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -26,7 +26,7 @@ class WoZaiXiaoYuanPuncher:
         self.header = {
             "Accept-Encoding": "gzip, deflate, br",
             "Connection": "keep-alive",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.13(0x18000d32) NetType/WIFI Language/zh_CN miniProgram",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800123c) NetType/WIFI Language/zh_CN",
             "Content-Type": "application/json;charset=UTF-8",
             "Content-Length": "2",
             "Host": "gw.wozaixiaoyuan.com",
